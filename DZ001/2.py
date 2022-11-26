8ee525ec497243f99403deef2fe8d685842711ae
Olga Koroleva <olga@Yurys-iMac.lan>
2022-11-26 16:28:59 +0300

    DZ001

---
+++ DZ001/2.py
@@ -0,0 +1,8 @@
+# 2. Напишите программу для. проверки истинности утверждения
+# ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+
+for x in range(0, 2):
+    for y in range(0, 2):
+        for z in range(0, 2):
+            print(f'x={x}, y={y}, z={z} {not(x or y or z) == ((not x) and (not y) and (not z))}')
+
